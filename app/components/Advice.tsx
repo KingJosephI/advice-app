43803ad1d9c90618d3b9ca166c17172255654400
King Joseph I <110317270+KingJosephI@users.noreply.github.com>
2023-04-22 22:48:18 +0100

    feat: display a new advice every 5 seconds

--- app/components/Advice.tsx
+++ app/components/Advice.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "@tanstack/react-query";
 import iconDice from "../mockup/icon-dice.svg";
 import dividerPattern from "../mockup/pattern-divider-desktop.svg";
 import axios from "axios";
+import { useRef } from "react";
 
 const fetchAdvice = async () => {
   const response = await axios.get("https://api.adviceslip.com/advice");
@@ -11,43 +12,33 @@ const fetchAdvice = async () => {
   return data;
 };
 const Advice = () => {
-  const { data, isLoading, isError } = useQuery(["advive"], fetchAdvice, {
-    staleTime: 1000,
+  const { data, isLoading, isError } = useQuery(["advice"], fetchAdvice, {
+    staleTime: 5000,
+    refetchInterval: 5000,
   });
   if (isLoading) return <h1>Loading...</h1>;
   if (isError) return <h1>Error while fetching data...</h1>;
 
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(data.slip.advice);
+  };
+
   return (
-    <article className="relative bg-grayishBlue rounded-[10px] w-[400px] h-[250px] px-12 py-6 text-center ">
+    <article className="relative bg-grayishBlue rounded-[10px] w-[400px] min-h-[250px] px-12 py-6 text-center ">
       <h1 className="uppercase text-neonGreen font-bold text-[11px]">
         Advice #{data.slip.id}
       </h1>
-      {/* 
-      .fancy span:before,
-.fancy span:after {
-  content: "";
-  position: absolute;
-  height: 5px;
-  border-bottom: 1px solid white;
-  border-top: 1px solid white;
-  top: 0;
-  width: 600px;
-}
-.fancy span:before {
-  right: 100%;
-  margin-right: 15px;
-}
-.fancy span:after {
-  left: 100%;
-  margin-left: 15px;
-}
-
-      */}
-      <p className="text-[#FFF] h-[150px] font-bold text-[20px] py-6">{`"${data.slip.advice}"`}</p>
-      <div className="divider">
+      <p className="text-[#FFF] min-h-[150px] font-bold text-[20px] py-6">
+        {isLoading ? <span>Loading...</span> : `"${data.slip.advice}"`}
+      </p>
+      <div className="mb-2">
         <Image src={dividerPattern} alt="" />
       </div>
-      <div className="absolute -bottom-[27px] cursor-pointer left-[calc(50%-48px)] right-[calc(50%-48px)]  h-[60px] w-[60px] grid place-content-center rounded-full mx-auto bg-neonGreen shadow-lg hover:shadow-neonGreen/80">
+
+      <div
+        onClick={copyToClipboard}
+        className="absolute -bottom-[27px] cursor-pointer left-[calc(50%-48px)] right-[calc(50%-48px)]  h-[60px] w-[60px] grid place-content-center rounded-full mx-auto bg-neonGreen shadow-lg hover:shadow-neonGreen/80"
+      >
         <Image src={iconDice} alt="" />
       </div>
     </article>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
 import Advice from "./components/Advice";
 
 const queryClient = new QueryClient();
